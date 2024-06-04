c42224b9027c3a365517451308631c9c874c72d5
Tech <debitcardzpaypal@gmail.com>
2024-06-04 13:10:49 -0400

    feat: maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
 }
+
+val githubActor: String = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+val githubToken: String = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
 
 group = "gg.ingot.iron"
 version = "1.0.0"
@@ -25,4 +29,30 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+
+// Allow for publishing to Maven local.
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/IngotGG/iron")
+            credentials {
+                username = githubActor
+                password = githubToken
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
